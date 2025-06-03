4c9c88722da97ce17ddfa2bc8b7bbe165f582c42
Valen-17 <valeperezcar2015@gmail.com>
2025-06-02 22:00:09 -0500

    actualizacion cliente y  proveedor

--- Proyecto_Stock_IQ/Clientes.cs
+++ Proyecto_Stock_IQ/Clientes.cs
@@ -262,12 +262,65 @@ namespace Proyecto_Stock_IQ
 
         private void guna2HtmlLabel2_Click(object sender, EventArgs e)
         {
-
+            FormInventario inventario = new FormInventario();
+            inventario.Show();
+            this.Hide(); // Oculta la ventana actual
         }
 
         private void guna2PictureBox3_Click(object sender, EventArgs e)
         {
+            FormInventario inventario = new FormInventario();
+            inventario.Show();
+            this.Hide(); // Oculta la ventana actual
+        }
 
+        private void guna2HtmlLabel6_Click(object sender, EventArgs e)
+        {
+            Proveedores proveedores = new Proveedores();
+            proveedores.Show();
+            this.Hide(); // Oculta la ventana actual
+        }
+
+        private void lbl_crearFractura_Click(object sender, EventArgs e)
+        {
+            CrearFactura crearFactura = new CrearFactura();
+            crearFactura.Show();
+            this.Hide(); // Oculta la ventana actual    
+        }
+
+        private void guna2PictureBox8_Click(object sender, EventArgs e)
+        {
+            CrearFactura crearFactura = new CrearFactura();
+            crearFactura.Show();
+            this.Hide(); // Oculta la ventana actual
+        }
+
+        private void lbl_hacerPedido_Click(object sender, EventArgs e)
+        {
+            HacerPedido hacerPedido = new HacerPedido();
+            hacerPedido.Show();
+            this.Hide(); // Oculta la ventana actual
+        }
+
+        private void guna2PictureBox6_Click(object sender, EventArgs e)
+        {
+            HacerPedido hacerPedido = new HacerPedido();
+            hacerPedido.Show();
+            this.Hide(); // Oculta la ventana actual
+        }
+
+        private void configuracion_Click(object sender, EventArgs e)
+        {
+            Configuracion configuracion = new Configuracion();
+            configuracion.Show();
+            this.Hide(); // Oculta la ventana actual
+        }
+
+        private void btn_configuracion_Click(object sender, EventArgs e)
+        {
+            Configuracion configuracion = new Configuracion();
+            configuracion.Show();
+            this.Hide(); // Oculta la ventana actual
         }
     }
 }
